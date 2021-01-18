8250e874c78bb5cefbafa597461e96b776b2590b
Kamil Postaagasi <kamilpostaagasi@gmail.com>
2021-01-19 00:14:31 +0300

    master

--- ClassMethodDemo/Program.cs
+++ ClassMethodDemo/Program.cs
@@ -6,45 +6,15 @@ namespace ClassMethodDemo
     {
         static void Main(string[] args)
         {
-            Musteri musteri1 = new Musteri();
-            musteri1.ID = 10567;
-            musteri1.AD = "Ahmet";
-            musteri1.SOYAD = "Hanoğlu";
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(12345, "Kamil Postaağası", "Erkek", 16);
+            musteriManager.Ekle(12346, "Mehmet Çetin", "Erkek", 18);
+            musteriManager.Ekle(12347, "Ahmet Öksüz", "Erkek", 54);
+            musteriManager.Ekle(12348, "Yaren Kurtulan", "Kadın", 36);
+            musteriManager.Ekle(12349, "Tülin Özer", "Kadın", 42);
 
-            Musteri musteri2 = new Musteri();
-            musteri2.ID = 12543;
-            musteri2.AD = "Buğra";
-            musteri2.SOYAD = "Tandoğan";
-
-            Musteri musteri3 = new Musteri();
-            musteri3.ID = 12456;
-            musteri3.AD = "Mehmet";
-            musteri3.SOYAD = "Çetin";
-
-            MusteriManager.MusteriEkle(musteri1);
-            MusteriManager.MusteriListele(musteri2);
-            MusteriManager.MusteriSil(musteri3);
-        }
-    }
-    class Musteri
-    {
-        public int ID { get; set; }
-        public string AD { get; set; }
-        public string SOYAD { get; set; }
-    }
-    class MusteriManager
-    {
-        public static void MusteriEkle(Musteri musteri)
-        {
-            Console.WriteLine(musteri.AD + " " + musteri.SOYAD + " sisteme eklendi.");
-        }
-        public static void MusteriListele(Musteri musteri)
-        {
-            Console.WriteLine("Müşteri Listelendi: " + musteri.ID + " " + musteri.AD + " " + musteri.SOYAD);
-        }
-        public static void MusteriSil(Musteri musteri)
-        {
-            Console.WriteLine(musteri.AD + " " + musteri.SOYAD + " sistemden silindi.");
+            musteriManager.Sil(12349);
+            musteriManager.Listele();
         }
     }
 }
